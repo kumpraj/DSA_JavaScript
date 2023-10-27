32a2ee8750facc5b639c911c566f8dc9a76f3cc2
Prajwal Kumar <kumpraj@gmail.com>
2023-10-27 12:16:47 +0530

    added remove method

--- LinkedList/LinkedList.js
+++ LinkedList/LinkedList.js
@@ -125,14 +125,33 @@ class LinkedList {
 
         return true;
     }
+
+    // to remove a node at a particular index
+    remove(index){
+        if(index === 0) return this.shift();
+        if(index === this.length-1) return this.pop();
+        if(index< 0 || index >= this.length) return undefined;
+
+        const before = this.get(index-1);
+        const temp = before.next;
+
+        before.next = temp.next;
+        temp.next = null;
+        this.length--;
+        return temp;
+    }
 }
 
 const myLinkedList = new LinkedList(0);
 // console.log(myLinkedList.pop())
 
 console.log(myLinkedList.push(1))
-console.log(myLinkedList.insert(2,2));
-// console.log(myLinkedList.push(9))
+// console.log(myLinkedList.insert(2,2));
+console.log(myLinkedList.push(9));
+console.log(myLinkedList.push(10));
+
+console.log(myLinkedList.remove(1));
+
 // console.log(myLinkedList.get(2));
 // console.log(myLinkedList.set(0,77));
 console.log(myLinkedList)
