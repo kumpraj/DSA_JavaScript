8acabb871359fb94aee397ec8ec60dd621ea3048
Prajwal Kumar <kumpraj@gmail.com>
2023-11-03 01:04:21 +0530

    BST constructor added

---
+++ BST/bst.js
@@ -0,0 +1,16 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BST {
+    constructor(){
+        this.root = null;
+    }
+}
+
+let myBst = new BST();
+console.log(myBst)
