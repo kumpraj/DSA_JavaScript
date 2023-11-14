933c48af1bd12333ead60b5868651ab6eef54e1f
Prajwal Kumar <kumpraj@gmail.com>
2023-11-14 13:26:45 +0530

    added Binary search tree with insert method

--- BST/bst.js
+++ BST/bst.js
@@ -10,7 +10,43 @@ class BST {
     constructor(){
         this.root = null;
     }
-}
+
+    insert(value){
+        const newNode = new Node(value);
+
+        if(this.root === null) {
+            this.root = newNode;
+            return this;
+        }
+
+        let temp = this.root;
+
+        while(true){
+            if (newNode.value === temp.value) return undefined;
+            
+            if(newNode.value < temp.value ){
+                if(temp.left === null){
+                    temp.left = newNode;
+                    return this;
+                }
+                temp = temp.left;
+            }else{
+                    if(temp.right === null){
+                        temp.right = newNode
+                        return this;
+                    }
+                    temp = temp.right;
+                }
+            }
+
+            return this;
+        }
+    }
+
 
 let myBst = new BST();
-console.log(myBst)
+myBst.insert(50);
+myBst.insert(45);
+myBst.insert(40);
+myBst.insert(55);
+console.log(myBst.insert(60));
