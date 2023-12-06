f1417d13ec4214f72d12587d1b1958be55025a3a
Prajwal Kumar <kumpraj@gmail.com>
2023-12-06 14:11:49 +0530

    added DoublyLinkedList with its constructor

---
+++ DoublyLinkedList/DoublyLinkedList.js
@@ -0,0 +1,18 @@
+class Node {
+    constructor(value){
+        this.value = value;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class DoublyLinkedList {
+    constructor(value){
+        const newNode = new Node(value);
+        this.head = newNode;
+        this.tail = newNode;
+    }
+}
+
+let myDLL = new DoublyLinkedList(7);
+console.log(myDLL);
