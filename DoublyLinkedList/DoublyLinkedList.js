2f0798b78d7b447f026538d2d95f1d6d0a9a958f
Prajwal Kumar <kumpraj@gmail.com>
2023-12-06 20:17:20 +0530

    added Doubly LinkedList with constructor

--- DoublyLinkedList/DoublyLinkedList.js
+++ DoublyLinkedList/DoublyLinkedList.js
@@ -11,8 +11,25 @@ class DoublyLinkedList {
         const newNode = new Node(value);
         this.head = newNode;
         this.tail = newNode;
+        this.length = 1;
+    }
+
+    push(value){
+        const newNode = new Node(value);
+        if(!this.head){
+            this.head = newNode
+            this.tail = newNode
+        }else{
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
     }
 }
 
-let myDLL = new DoublyLinkedList(7);
+let myDLL = new DoublyLinkedList(1);
 console.log(myDLL);
+console.log(myDLL.push(2));
+console.log(myDLL.push(3));
